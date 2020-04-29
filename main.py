f30978eedcfaa391503e9d601b5d1a6cef89c6e1
Pablo Jimenez <pablo.jimenezmateo@imdea.org>
2020-04-29 18:34:09 +0200

    Started working on the text searcher

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ import re
 # For audio playing
 from pygame import mixer
 
+# For text indexing
+from whoosh import index
+from whoosh.fields import Schema, ID, TEXT
+
 # Global definitions
 # This is the path where notes will be stored and read from
 hobbes_db = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'db')
@@ -52,6 +56,101 @@ hobbes_db = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'db')
         - Implement contextual menu options
 
 '''
+
+
+'''
+    From the Whoosh docs, incremental indexing of the files
+'''
+def index_my_docs(db_path, dirname, clean=False):
+
+    # If the index folder does not exist create it
+    if not os.path.exists(os.path.join(db_path, dirname)):
+
+        os.mkdir(os.path.join(db_path, dirname))
+
+
+    if clean:
+
+        clean_index(db_path, dirname)
+
+    else:
+
+        incremental_index(db_path, dirname)
+
+def clean_index(db_path, dirname):
+    # Always create the index from scratch
+    ix = index.create_in(os.path.join(db_path, dirname), schema=get_schema())
+    writer = ix.writer()
+
+    # Assume we have a function that gathers the filenames of the
+    # documents to be indexed
+    for path in my_docs(db_path):
+        add_doc(writer, path)
+
+    writer.commit()
+
+def my_docs(db_path):
+
+    # Get all the txt files from a given path
+    result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(db_path) for f in filenames if os.path.splitext(f)[1] == '.txt']
+    return result
+
+def get_schema():
+
+    return Schema(path=ID(unique=True, stored=True), title=TEXT(stored=True), content=TEXT)
+
+def add_doc(writer, path):
+
+    fileobj = open(path, "rb")
+    content = fileobj.read()
+    fileobj.close()
+    title = os.path.basename(path).split(".")[0]
+    writer.add_document(path=path, content=content.decode("utf-8", "strict"), title=title)
+
+def incremental_index(db_path, dirname):
+
+    ix = index.open_dir(os.path.join(db_path, dirname))
+
+    # The set of all paths in the index
+    indexed_paths = set()
+
+    # The set of all paths we need to re-index
+    to_index = set()
+
+    with ix.searcher() as searcher:
+        writer = ix.writer()
+
+        # Loop over the stored fields in the index
+        for fields in searcher.all_stored_fields():
+            indexed_path = fields['path']
+            indexed_paths.add(indexed_path)
+
+        if not os.path.exists(indexed_path):
+            # This file was deleted since it was indexed
+            writer.delete_by_term('path', indexed_path)
+
+        else:
+            # Check if this file was changed since it
+            # was indexed
+            indexed_time = fields['time']
+            mtime = os.path.getmtime(indexed_path)
+
+            if mtime > indexed_time:
+                # The file has changed, delete it and add it to the list of
+                # files to reindex
+                writer.delete_by_term('path', indexed_path)
+                to_index.add(indexed_path)
+
+        # Loop over the files in the filesystem
+        # Assume we have a function that gathers the filenames of the
+        # documents to be indexed
+        for path in my_docs(db_path):
+            if path in to_index or path not in indexed_paths:
+                # This is either a file that's changed, or a new file
+                # that wasn't indexed before. So index it!
+                add_doc(writer, path)
+
+        writer.commit()
 
 """ Sort the given iterable in the way that humans expect.""" 
 def sorted_nicely(l): 
@@ -753,6 +852,11 @@ class MainScreen(BoxLayout):
         ## Push everything
         #origin.push()
 
+        '''
+            Indexing test
+        '''
+        index_my_docs(os.path.join(hobbes_db, 'Work'), '.text_index', True)
+
 
     # This method will be in charge of all the input actions
     def on_touch_down(self, touch):
@@ -785,6 +889,24 @@ class MainScreen(BoxLayout):
 
             self.note_text_input.save_note()
 
+        elif 'ctrl' in modifier and codepoint == 'g':
+
+            # Create the searcher
+            from whoosh.qparser import MultifieldParser
+
+            ix = index.open_dir(os.path.join(hobbes_db, 'Work/.text_index'))
+
+            with ix.searcher() as searcher:
+
+                parser = MultifieldParser(["title", "content"], schema=ix.schema)
+
+                query = parser.parse(u"rando*")
+                results = searcher.search(query, terms=True)
+
+                for hit in results:
+
+                    print(hit)
+                    print(hit.matched_terms())
 
 class HobbesApp(App):
 

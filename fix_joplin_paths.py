c0bbb69ae933d962dd5aca1e2875706aa8c63089
Pablo Jimenez <pablo.jimenezmateo@imdea.org>
2020-05-13 14:11:38 +0200

    Deleted unused file

--- fix_joplin_paths.py
+++
@@ -1,8 +0,0 @@
-import os
-
-dirs = ["Addendum", "Bookmarks", "IMDEA", "Miscellaneous", "Personal", "Personal projects", "Quotes", "Technology", "Vegan"]
-
-for d in dirs:
-    for path, subdirs, files in os.walk(dirs):
-        for name in files:
-            print(os.path.join(path, name))
